3e46f531d3f3dbfae3beec61db06360159d47d86
LamFoxer <dakfor@yandex.ru>
2025-03-12 21:39:33 +0300

    screen1 in work

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,6 +1,61 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    function handleMouseMove(event){
+    const nose = document.querySelector('.noseLeft');
+    const nose2 = document.querySelector('.noseRight');
+    const beigeCircle = document.querySelector('.beigeCircle');
+
+    nose.addEventListener('click', () => {
+        // Анимация красного носа
+        nose.style.animationPlayState = 'paused';
+        nose.style.animation = 'shake 0.5s ease-in-out';
+        nose.style.borderBottom = '4.8vw solid #a92724';
+
+        setTimeout(() => {
+            nose.style.animation = '';
+            nose.style.borderBottom = '4.8vw solid #EC4E4C';
+        }, 500);
+
+        setTimeout(() => {
+            nose.style.animationPlayState = 'running';
+        }, 3000);
+
+        // Анимация бежевого круга
+        beigeCircle.style.animation = 'shake 0.5s ease-in-out';
+        beigeCircle.style.backgroundColor = '#EC4E4C';
+
+        setTimeout(() => {
+            beigeCircle.style.animation = '';
+            beigeCircle.style.backgroundColor = '';
+        }, 500);
+    });
+
+    nose2.addEventListener('click', function() {
+        // Анимация синего носа
+        nose2.style.borderBottom = '3.2vw solid #8895fd';
+
+        setTimeout(() => {
+            nose2.style.borderBottom = '3.2vw solid #5065FF';
+        }, 500);
+    });
+
+// АНИМАЦИЯ ГЛАЗ
+
+    let eye = document.querySelectorAll(".eyeLeft");
+    
+    window.addEventListener("mousemove", (e) => {
+        eye.forEach(function(eye) {
+            let rect = eye.getBoundingClientRect();
+            let x = (e.pageX - rect.left) / 50 + "px";
+            let y = (e.pageY - rect.top) / 50 + "px";
+            eye.style.transform = "translate3d(" + x + "," + y + ", 0px)";
+        });
+    });
+    
+// АНИМАЦИЯ БУКВ
+
+    let letterS = document.querySelector(".letterS");
+
+    /*function handleMouseMove(event){
         let eyes = document.querySelectorAll(".eye");
         eyes.forEach(function(eye){
             let rect = eye.getBoundingClientRect();
@@ -25,5 +80,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
         document.addEventListener("mousemove", cursorColor);
 
-    }
+    }*/
 });
